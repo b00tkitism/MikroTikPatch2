91ebe7fd37f785c3270b39713e895ef6138c0aa4
b00tkitism <119131616+b00tkitism@users.noreply.github.com>
2025-02-14 18:55:12 +0000

    Update patch.py
    Signed-off-by: b00tkitism <119131616+b00tkitism@users.noreply.github.com>

--- patch.py
+++ patch.py
@@ -1,4 +1,4 @@
-import subprocess,lzma
+_import subprocess,lzma
 import struct,os,re
 from npk import NovaPackage,NpkPartID,NpkFileContainer
 
@@ -275,10 +275,10 @@ def patch_squashfs(path,key_dict):
                     if _data != data:
                         open(file,'wb').write(_data)
                 url_dict = {
-                    os.environ['MIKRO_LICENCE_URL'].encode():os.environ['CUSTOM_LICENCE_URL'].encode(),
-                    os.environ['MIKRO_UPGRADE_URL'].encode():os.environ['CUSTOM_UPGRADE_URL'].encode(),
-                    os.environ['MIKRO_CLOUD_URL'].encode():os.environ['CUSTOM_CLOUD_URL'].encode(),
-                    os.environ['MIKRO_CLOUD_PUBLIC_KEY'].encode():os.environ['CUSTOM_CLOUD_PUBLIC_KEY'].encode(),
+                    # os.environ['MIKRO_LICENCE_URL'].encode():os.environ['CUSTOM_LICENCE_URL'].encode(),
+                    # os.environ['MIKRO_UPGRADE_URL'].encode():os.environ['CUSTOM_UPGRADE_URL'].encode(),
+                    # os.environ['MIKRO_CLOUD_URL'].encode():os.environ['CUSTOM_CLOUD_URL'].encode(),
+                    # os.environ['MIKRO_CLOUD_PUBLIC_KEY'].encode():os.environ['CUSTOM_CLOUD_PUBLIC_KEY'].encode(),
                 }
                 data = open(file,'rb').read()
                 for old_url,new_url in url_dict.items():
@@ -289,7 +289,7 @@ def patch_squashfs(path,key_dict):
                         
                 if os.path.split(file)[1] == 'licupgr':
                     url_dict = {
-                        os.environ['MIKRO_RENEW_URL'].encode():os.environ['CUSTOM_RENEW_URL'].encode(),
+                        # os.environ['MIKRO_RENEW_URL'].encode():os.environ['CUSTOM_RENEW_URL'].encode(),
                     }
                     for old_url,new_url in url_dict.items():
                         if old_url in data:
